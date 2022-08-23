f20b2cd9e97be8facc8b5581fdd527d9c5ba5b15
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-23 23:23:56 +0100

    Added Destination and Data component, then Styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar.jsx";
 import Destinations from "./Destinations.jsx";
+import destinationsData from "./Data.jsx";
 
 const App = () => {
+  const destinationsElements = destinationsData.map((destination) => {
+    return <Destinations key={destination.id} {...destination} />;
+  });
   return (
     <div className="container">
-      <Navbar />
-      <Destinations />
+      <Navbar className="des--list" />
+      <section>{destinationsElements}</section>
     </div>
   );
 };
